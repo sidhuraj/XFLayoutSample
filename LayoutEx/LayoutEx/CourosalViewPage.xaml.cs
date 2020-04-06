57f663542b9e0b09705b5b0bb8504ec7ce644b39
sidhuraj <mr.prvsantosh@rediffmail.com>
2020-04-06 16:20:24 +0530

    first commit

---
+++ LayoutEx/LayoutEx/CourosalViewPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutEx
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CourosalViewPage : ContentPage
+    {
+        public CourosalViewPage()
+        {
+            InitializeComponent();
+
+            List<State> objStatesList = new List<State>();
+            objStatesList.Add(new State { StatePhoto = "icon.png", StateName = "Telangana" });
+            objStatesList.Add(new State { StatePhoto = "icon.png", StateName = "Andrapradesh" });
+            objStatesList.Add(new State { StatePhoto = "icon.png", StateName = "Kerala" });
+            objStatesList.Add(new State { StatePhoto = "icon.png", StateName = "Odisha" });
+            objStatesList.Add(new State { StatePhoto = "icon.png", StateName = "Karnataka" });
+            objStatesList.Add(new State { StatePhoto = "icon.png", StateName = "Tamilnadu" });
+            objStatesList.Add(new State { StatePhoto = "icon.png", StateName = "Telangana" });
+            cvStatesList.ItemsSource = objStatesList;
+
+        }
+    }
+}
---
+++ LayoutEx/LayoutEx/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LayoutEx
+{
+   public class State
+    { 
+        public string StatePhoto { set; get; }
+        public string StateName { set; get; }
+    }
+}
